e938f373d3fe764406454cf1d79d71ffc226491c
dhairyakaneria <dhairya.kaneria@gmail.com>
2025-12-29 13:27:05 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,227 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "6662e7d5-c555-4a35-a154-ce9fffd1412b",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "10\n"
+     ]
+    }
+   ],
+   "source": [
+    "x=10\n",
+    "print(x)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "d6b45d6a-f589-4dff-932a-ed446fe659d5",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "-5\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=5\n",
+    "print(-a)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "19ca155e-0936-4596-9b08-52095f2af52d",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "True\n",
+      "False\n"
+     ]
+    }
+   ],
+   "source": [
+    "p=10\n",
+    "q=5\n",
+    "print(p>q)\n",
+    "print(p==q)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "4c7dc229-2904-4ccd-bed2-5ecc99ccbc01",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "False\n",
+      "True\n",
+      "False\n"
+     ]
+    }
+   ],
+   "source": [
+    "x = True\n",
+    "y = False\n",
+    "print(x and y)\n",
+    "print(x or y)\n",
+    "print(not x)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "0e221d42-59e5-4d03-a5b0-d97ac6d9e4c5",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "True\n"
+     ]
+    }
+   ],
+   "source": [
+    "is_pass = True\n",
+    "print(is_pass)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "id": "136f5311-e10f-4fe2-a4f8-4017a678adab",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "1\n",
+      "7\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=5\n",
+    "b=3\n",
+    "print(a&b)\n",
+    "print(a|b)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "f6abc26f-af8f-40aa-8fac-19e9d7ffc047",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "True\n",
+      "False\n"
+     ]
+    }
+   ],
+   "source": [
+    "number=[1,2,3,4,5,6]\n",
+    "print(2 in number)\n",
+    "print(5 not in number)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "id": "cb3b67b6-6593-47f8-979a-1fb7b2f8b8d6",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "True\n",
+      "False\n"
+     ]
+    }
+   ],
+   "source": [
+    "x=10\n",
+    "y=10\n",
+    "print(x is y)\n",
+    "print(x is not y)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "id": "e3427c21-0740-4a57-8e0f-585d895177b7",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "15\n",
+      "5\n",
+      "50\n",
+      "2.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=10\n",
+    "b=5\n",
+    "print(a+b)\n",
+    "print(a-b)\n",
+    "print(a*b)\n",
+    "print(a/b)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "0312837d-77cc-4c19-a697-66658cb73079",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.14.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
