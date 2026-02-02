e40f8726b242e33bfb93e2d0b0ce63b0204a8ef5
dhairyakaneria <dhairya.kaneria@gmail.com>
2026-02-02 10:33:03 +0530

    Add files via upload

---
+++ input_output_statements.py
@@ -0,0 +1,39 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #input & output
+>>> #i INTEGER
+>>> x = 20
+>>> print(x)
+20
+>>> a = 40
+>>> print(a)
+40
+>>> # FLOAT
+>>> price = float(input("Enter price: "))
+Enter price: 45.26
+>>> print ("Print is:",price)
+Print is: 45.26
+>>> x = float(input("Enter a decimal number: "))
+Enter a decimal number: 20.85
+>>> print("Square =, x * x")
+Square =, x * x
+>>> print("Square =", x * x)
+Square = 434.7225000000001
+>>> # BOOLEAN
+>>> is_student = input("Are you a student (True/False): ") =="True"
+Are you a student (True/False): True
+>>> print("Student status:", is_student)
+Student status: True
+>>> light_on = input("Is the light on (True/False): ") =="True"
+Is the light on (True/False): False
+>>> print("Light on:", light_on)
+Light on: False
+>>> # STRING
+>>> name = input("Enter your name: ")
+Enter your name: Dhairya
+>>> print("Helo,",name)
+Helo, Dhairya
+>>> city = input("Enter your city: ")
+Enter your city: Rajkot
+>>> print("You live in", city)
+You live in Rajkot
