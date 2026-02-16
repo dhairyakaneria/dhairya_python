d93be74524089801f6a7e40645e57bd33847c268
dhairyakaneria <dhairya.kaneria@gmail.com>
2026-02-16 10:39:45 +0530

    Add files via upload

---
+++ For_Loop.py
@@ -0,0 +1,40 @@
+# For Loop Programs
+# 1. Print numbers from 1 to 10
+"""for i in range (1,11):
+    print (i)"""
+# 2. Print Even numbers from 1 to 20
+"""for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)"""
+ # 3. Print odd numbers from 1 to 15
+"""for i in range (1,16):
+    if i % 2 != 0:
+        print (i)"""
+
+# 4. Print Table of 5
+"""for i in range(1, 11):
+    print(5 * i)"""
+
+# 5. Print Character of a string
+name = "Dhairya"
+"""for letter in name:
+    print(letter)"""
+
+# 6. Sum of number from 1 to 5
+"""total = 0
+for i in range(1,6):
+    total = total + i
+    print ("Sum is: ", total)"""
+
+# 7. Print list Elements
+"""numbers = [10,20,30,40]
+for n in numbers:
+    print (n)"""
+
+# 8. Print number 1 to 5
+"""for i in range(1,6):
+    print (i)"""
+
+# 9. Print a message 3 times
+for i in range (3):
+    print ("Hello")
